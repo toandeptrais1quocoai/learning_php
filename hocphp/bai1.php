3f34d2dd6a79659bd603eae68b44d2aae4105765
Toandeptrais1quocoai <tn5187519@gmail.com>
2025-12-06 23:26:13 +0700

    Add hocphp folder

---
+++ hocphp/bai1.php
@@ -0,0 +1,26 @@
+<html><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Taif lieeuj laapj trinhf wweb</h1>
+    <?php
+        echo "Hello World!";
+    ?>
+    <p>Taif Lieeuj HTML</p>
+    <p>tai lieu csss</p>
+    <?php
+        echo "Hello CSS!";
+        echo "<br>";
+        echo "Hello JavaScript!";
+    ?>
+    <hr>
+    <?php
+    $text = "tu co ban" . " " . " ddeens naang cao0";
+    echo $text;
+    ?>
+</body>
+</html></html>
