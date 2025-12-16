7ac537ac4cfd5d5b52791bfef739ad379cbd1219
Toandeptrais1quocoai <tn5187519@gmail.com>
2025-12-16 20:17:00 +0700

    bai tap ngay 10-12-2025

---
+++ hocphp/bai6.php
@@ -0,0 +1,14 @@
+<html><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="bai6.php" method ="GET">
+    <input type="text" name = "search">
+    <button type ="submit">Tìm kiếm</button>
+    </form>
+</body>
+</html></html>
